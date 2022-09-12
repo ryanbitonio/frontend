6ee23233efe07df7e1ddfc7999dc64675dd9bd43
Ryan <ryanbitonio.ayang@gmail.com>
2022-09-12 22:16:39 +0800

    Deployment

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  async redirects() {
+    return [{ source: "/canceled", destination: "/", permanent: true }];
+  },
+};
+
+module.exports = nextConfig;
